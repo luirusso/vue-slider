2f0861d89dfb2f1cb8cb0ea2bf2c410596a34cc0
luirusso <luirusso_@hotmail.it>
2021-11-12 13:06:16 +0100

    project setup

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/**
+ * Descrizione:
+Partendo dalla struttura base, allegata sotto, rifare l’esercizio dello slider come fatto insieme in classe.
+In allegato con la base con HTML e CSS trovate anche un file con i dati necessari per lo slider.
+Bonus
+Applicare l’autoplay allo slider: ogni 3 secondi cambia immagine automaticamente.
+Docs
+Introduzione: https://vuejs.org/v2/guide/index.html
+Conditionals e loops: https://vuejs.org/v2/guide/index.html#Conditionals-and-Loops
+Class binding (toggle dinamico di una classe): https://vuejs.org/v2/guide/class-and-style.html
+
+ */
+
+
+/*
+ * Vue Slider
+ */
+
